1d05284a0f57fe5c9050c31c654d7cac7ac18582
Quentin_Chen <quentin.chen@foxmail.com>
2016-11-15 17:38:55 -0800

    update producer.py

--- script/producer.py
+++ script/producer.py
@@ -4,12 +4,12 @@ from kafka import KafkaProducer
 import json
 
 
-if __name__ == '__main__':
-    main()
-
-
 def main():
     producer = KafkaProducer(
     	value_serializer=lambda v: json.dumps(v).encode('utf-8')
     	)
     producer.send('test', {'foo': 'bar'})
+
+
+if __name__ == '__main__':
+    main()
