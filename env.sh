623ec655f603ad82f86424d826270315b516fecc
Quentin_Chen <quentin.chen@foxmail.com>
2016-11-15 16:46:19 -0800

    add example.py

--- env.sh
+++ env.sh
@@ -1,12 +1,8 @@
-echo "===Begin to set kafka test environment.==="
-
 export KAFKA_HOME="/home/cloudera/opt/kafka_2.10-0.9.0.1"
 
-alias kafka-start-server="$KAFKA_HOME/bin/kafka-server-start.sh ./config/server.properties"
+alias kafka-start-server="$KAFKA_HOME/bin/kafka-server-start.sh $(dirname $0)/config/server.properties"
 alias kafka-topic-list="$KAFKA_HOME/bin/kafka-topics.sh --list --zookeeper localhost:2181"
 alias kafka-topic-create="$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic"
 alias kafka-topic-delete="$KAFKA_HOME/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic"
 alias kafka-console-producer="$KAFKA_HOME/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic"
 alias kafka-console-consumer="$KAFKA_HOME/bin/kafka-console-consumer.sh --zookeeper localhost:2181 --from-beginning --topic"
-
-echo "===Environment done.==="
---
+++ script/prod_cons_example.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import threading, logging, time
+from kafka import KafkaConsumer, KafkaProducer
+
+
+class Producer(threading.Thread):
+    daemon = True
+
+    def run(self):
+        producer = KafkaProducer(bootstrap_servers='localhost:9092')
+        message = "Test message from python-kafka-plugin."
+
+        while True:
+            producer.send('test', message)
+            print("Sent:", message)
+            time.sleep(5)
+
+
+class Consumer(threading.Thread):
+    daemon = True
+
+    def run(self):
+        consumer = KafkaConsumer(bootstrap_servers='localhost:9092',
+                                 auto_offset_reset='earliest')
+        consumer.subscribe(['test'])
+
+        for message in consumer:
+            print (message)
+
+
+def main():
+    threads = [
+        Producer(),
+        Consumer()
+    ]
+
+    for t in threads:
+        t.start()
+
+    time.sleep(10)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format='%(asctime)s.%(msecs)s:%(name)s:%(thread)d' +
+        ':%(levelname)s:%(process)d:%(message)s',
+        level=logging.INFO
+    )
+    main()
